9009c0956699ea20c8ef5002f735805cc1690a6e
kachanovskyi <vova.kachanovsky@gmail.com>
2017-05-19 19:10:57 +0300

    finished index page. Started working on about page

--- js/index.js
+++ js/index.js
@@ -10,3 +10,33 @@ $('a.page-scroll').click(function () {
         }
     }
 });
+
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+
+    var paraxifyInit = function () {
+        myParaxify = paraxify('.paraxify');
+    };
+
+    var loadScript = function(url, callback) {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { //Others
+            script.onload = function () {
+                callback();
+            };
+        }
+
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    };
+
+    loadScript("./js/paraxify.min.js", paraxifyInit);
+}
